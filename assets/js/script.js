d0d179391b6faf37c9366d7162b792aeba1159f8
AzizTeqnite123 <azizteqnite@gmail.com>
2025-09-17 21:37:49 +0500

    initals

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const drawer = document.querySelector(".headerLinksMobileDrawer");
     const openBtn = document.querySelector(".menuDrawerOpen");
     const closeBtn = document.querySelector(".menuDrawerClose");
+    const overlay = document.querySelector(".mobileDrawrLayer"); 
 
     openBtn.addEventListener("click", () => {
         drawer.classList.add("active");
+        overlay.classList.add("active");
     });
 
     closeBtn.addEventListener("click", () => {
         drawer.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+
+    overlay.addEventListener("click", () => {
+        drawer.classList.remove("active");
+        overlay.classList.remove("active");
     });
 });
